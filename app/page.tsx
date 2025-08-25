e0101db654a4b895912ceb5bbac34dd95eb0312c
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-08-25 11:17:21 +0200

    Extract home page Header into component for readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
-import Image from 'next/image';
+import HomeHeader from '@/components/Home/Header';
 
 export default function Home() {
   return (
     <main className="content-grid">
-      <section className="content-grid full-width items-center gap-4 bg-gradient-to-br [background-image:linear-gradient(-10deg,_#C97FE4,_#AECDF6)] p-14">
-        <h1 className="text-center mt-14 text-8xl font-extrabold text-transparent bg-gradient-to-r from-purple-800 to-blue-800 [background-clip:text]">
-          Gotta catch &apos;em all!
-        </h1>
-        <p className="text-center text-white text-xl">
-          Discover, search and explore the amazing world of Pokémon. Find
-          <br /> your favourite and learn about their stats.
-        </p>
-        <button className="btn-primary">
-          <Image src="/Dice.svg" width={25} height={25} alt="Dice" />
-          Random Pokémon
-        </button>
-      </section>
+      <HomeHeader />
     </main>
   );
 }
