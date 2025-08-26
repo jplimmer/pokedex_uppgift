a26f9796bb7db93b3cd40f89b2317d614077d1d5
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-08-26 14:39:26 +0200

    Changed pages.json to ROUTES object constant

---
+++ src/lib/routes.ts
@@ -0,0 +1,18 @@
+export const routes = {
+  home: {
+    href: '/',
+    label: 'Home',
+  },
+  pokedex: {
+    href: '/pokedex',
+    label: 'Pokédex',
+  },
+  types: {
+    href: '/types',
+    label: 'Types',
+  },
+  favourites: {
+    href: '/favourites',
+    label: 'Favourites',
+  },
+} as const;
