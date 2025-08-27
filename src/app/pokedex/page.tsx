62478b4b5590fd9176dc455c6e64059424cd552b
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-08-27 14:36:46 +0200

    Add filter with page display limit to Pokedex page

--- src/app/pokedex/page.tsx
+++ src/app/pokedex/page.tsx
@@ -1,28 +1,52 @@
+import FilterBar from '@/components/FilterBar';
 import PokemonCard from '@/components/PokemonCard';
-import { fetchAllPokemon } from '@/lib/pokemon';
+import { fetchAllPokemon, fetchPokemonData } from '@/lib/pokemon';
 import { routes } from '@/lib/routes';
 import Link from 'next/link';
 
-export default async function PokedexPage() {
-  const pokemonList = await fetchAllPokemon();
+export default async function PokedexPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const { query = '', limit, page } = await searchParams;
 
+  // Query all Pokemon
+  const pokemonList = await fetchAllPokemon();
   if (!pokemonList) return;
 
+  const matchesList = pokemonList.filter((p) => p.name.includes(query));
+
+  // Limit data all and display with pagination
+  const currentPage = Number(page) || 1;
+  const pageLimit = Number(limit) || 20;
+  const offset = (currentPage - 1) * pageLimit;
+
+  const limitedList = matchesList.slice(offset, offset + pageLimit);
+
+  const matchesData = await fetchPokemonData(limitedList);
+
   return (
-    <div className="content-grid full-width [background-image:linear-gradient(-10deg,_#f5e6fb,_#eef5fd)] p-8 space-y-8">
-      <h2 className="text-4xl text-center">Pokédex</h2>
-      <ul className="grid grid-cols-[repeat(auto-fit,minmax(25ch,1fr))] content-stretch gap-4">
-        {pokemonList.map(
-          (p, i) =>
-            p.id && (
-              <li key={i} className="w-full justify-items-center">
-                <Link href={`${routes.pokedex.href}/${p.id}`}>
-                  <PokemonCard id={p.id} className="bg-white" />
-                </Link>
-              </li>
-            )
-        )}
-      </ul>
+    <div className="content-grid full-width [background-image:linear-gradient(-10deg,_#f5e6fb,_#eef5fd)] py-8">
+      <div className="grid grid-rows-[auto_auto_1fr] space-y-8">
+        <h2 className="text-4xl text-center">Pokédex</h2>
+        <FilterBar
+          placeholder="Search Pokémon..."
+          className="w-[50%] mx-auto"
+        />
+        <ul className="grid grid-cols-[repeat(auto-fit,minmax(25ch,1fr))] gap-4">
+          {matchesData.map(
+            (p, i) =>
+              p.id && (
+                <li key={i} className="w-full justify-items-center">
+                  <Link href={`${routes.pokedex.href}/${p.id}`}>
+                    <PokemonCard pokemon={p} className="bg-white" />
+                  </Link>
+                </li>
+              )
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/lib/pokemon.ts
+++ src/lib/pokemon.ts
@@ -10,8 +10,13 @@ export async function getNumberOfPokemon() {
   return 1025;
 }
 
-export async function fetchAllPokemon(limit = 20, offset = 0) {
-  const queryString = `?limit=${limit}&offset=${offset}`;
+export async function fetchAllPokemon(limit?: number, offset = 0) {
+  let queryLimit = limit;
+
+  if (!queryLimit) {
+    queryLimit = await getNumberOfPokemon();
+  }
+  const queryString = `?limit=${queryLimit.toString()}&offset=${offset.toString()}`;
   const response = await fetch(pokemonUrl + queryString);
 
   if (response.status !== 200) {
@@ -21,10 +26,10 @@ export async function fetchAllPokemon(limit = 20, offset = 0) {
   const { results: pokemonList }: { results: PokemonListItem[] } =
     await response.json();
 
-  for (const p of pokemonList) {
-    const id = getIdfromPokemonUrl(p.url);
-    p['id'] = id;
-  }
+  // for (const p of pokemonList) {
+  //   const id = getIdfromPokemonUrl(p.url);
+  //   p['id'] = id;
+  // }
 
   return pokemonList;
 }
@@ -53,19 +58,23 @@ export async function fetchPokemonBySearchParam(identifier: string) {
   return id;
 }
 
-export async function getRandomPokemonIds(number: number) {
-  const total = await getNumberOfPokemon();
-
+export async function getRandomPokemon(number: number) {
   const pokemonIds = new Set<string>();
+  const pokemonList: Pokemon[] = [];
+
+  const total = await getNumberOfPokemon();
 
   while (pokemonIds.size < number) {
     const randomInt = Math.floor(Math.random() * total) + 1;
+    // Check page exists before adding to set
     const res = await fetchPokemonById(randomInt.toString());
-    if (res) pokemonIds.add(randomInt.toString());
+    if (res) {
+      pokemonIds.add(randomInt.toString());
+      pokemonList.push(res);
+    }
   }
 
-  console.log('Selected IDs:', pokemonIds);
-  return Array.from(pokemonIds);
+  return pokemonList;
 }
 
 export function getIdfromPokemonUrl(url: string) {
@@ -79,4 +88,25 @@ export function getStatValue(pokemon: Pokemon, statName: string) {
     (s) => s.stat.name === statName.toLowerCase()
   );
   return stat?.base_stat ?? null;
+}
+
+export async function fetchPokemonData(
+  list: PokemonListItem[]
+): Promise<Pokemon[]> {
+  const fetchPromises = list.map((item) => fetch(item.url));
+
+  try {
+    const responses = await Promise.all(fetchPromises);
+
+    if (responses.some((res) => !res.ok)) {
+      throw new Error('One or more requests failed');
+    }
+
+    const jsonPromises = responses.map((res) => res.json());
+    const pokemonData: Pokemon[] = await Promise.all(jsonPromises);
+    return pokemonData;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
