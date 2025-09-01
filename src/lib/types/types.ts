ff1f096e942eb960d9a1add685cfacd3a87826f8
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-01 16:56:21 +0200

    Renamed GroupResultItem to match API docs, and moved to lib/types given broader use

---
+++ src/lib/types/types.ts
@@ -0,0 +1,35 @@
+export interface NamedAPIResource {
+  name: string;
+  url: string;
+}
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  sprites: {
+    primary: string;
+  };
+  stats: {
+    hp: string;
+    attack: string;
+    defense: string;
+    specialAttack?: string;
+    specialDefense?: string;
+    speed?: string;
+    accuracy?: string;
+    evasion?: string;
+  };
+  primaryType: TypeColour;
+  types: TypeColour[];
+}
+
+export interface TypeColour {
+  name: string;
+  colour: string;
+}
+
+export interface PokemonType extends TypeColour {
+  id: number;
+  pokemon: NamedAPIResource[];
+  sprite: string;
+}
