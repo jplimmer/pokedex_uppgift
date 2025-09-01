b7824a429b09d697380324c725efc9f214e38370
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-01 12:55:03 +0200

    Extract interface ffor Stat

--- src/lib/data/rest-api/types.ts
+++ src/lib/data/rest-api/types.ts
@@ -3,18 +3,20 @@ export interface GroupResultItem {
   url: string;
 }
 
+export interface Stat {
+  base_stat: number;
+  stat: {
+    name: string;
+  };
+}
+
 export type PokemonResultItem = {
   id: number;
   name: string;
   sprites: {
     front_default: string;
   };
-  stats: Array<{
-    base_stat: number;
-    stat: {
-      name: string;
-    };
-  }>;
+  stats: Stat[];
   types: {
     slot: number;
     type: GroupResultItem;
