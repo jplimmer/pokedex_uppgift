4913b12decbf81bd2e1f887dfd21172374f44a8a
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-04 09:40:53 +0200

    Simplified LoadingSpinner for reusability

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,11 @@ import { LoadingSpinner } from '@/components/loading';
 
 export default function Loading() {
   return (
-    <div className="content-grid full-width bg-neutral-100">
-      <LoadingSpinner />
+    <div className="content-grid full-width items-center justify-items-center bg-neutral-100">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <LoadingSpinner width={200} />
+        <p className="text-3xl">Loading...</p>
+      </div>
     </div>
   );
 }
--- src/components/loading/loading-spinner.tsx
+++ src/components/loading/loading-spinner.tsx
@@ -1,17 +1,14 @@
 import { ASSET_PATHS } from '@/lib/constants';
 import Image from 'next/image';
 
-export function LoadingSpinner() {
+export function LoadingSpinner({ width = 200 }: { width?: number }) {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <Image
-        src={ASSET_PATHS.LOADING}
-        alt=""
-        width={200}
-        height={200}
-        className="animate-spin"
-      />
-      <p className="text-3xl">Loading...</p>
-    </div>
+    <Image
+      src={ASSET_PATHS.LOADING}
+      alt=""
+      width={width}
+      height={width}
+      className="animate-spin"
+    />
   );
 }
