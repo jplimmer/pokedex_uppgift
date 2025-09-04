03e7b319211de510eb1fb93e2d458ffcdc7aa00c
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-04 23:33:05 +0200

    Added skeletons for TypeFilterList and fixed content-grid sizing

--- src/components/pokemon/type-filter-list.tsx.tsx
+++ src/components/pokemon/type-filter-list.tsx.tsx
@@ -10,7 +10,7 @@ export async function TypeFilterList() {
   const types = typesResult.data;
 
   return (
-    <ul className="grid grid-cols-[repeat(auto-fit,minmax(11ch,1fr))] gap-4 justify-items-center">
+    <ul className="grid grid-cols-[repeat(auto-fit,minmax(6rem,1fr))] gap-4 place-items-center">
       {types.map((type, index) => (
         <li key={index}>
           <TypeFilterButton pokemonType={type} />
