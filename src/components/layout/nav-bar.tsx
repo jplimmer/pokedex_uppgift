25f6247a33d1e32ee9f40e91085bc107971294a4
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-05 00:31:55 +0200

    Added current page indicator to Navbar

--- src/components/layout/nav-bar.tsx
+++ src/components/layout/nav-bar.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import { ASSET_PATHS, ROUTES } from '@/lib/constants';
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 export function NavBar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex items-center justify-between">
       <Link href={'/'} className="flex items-center gap-3 h-full py-4">
@@ -12,16 +17,22 @@ export function NavBar() {
         </span>
       </Link>
       <ul className="flex h-full">
-        {Object.values(ROUTES).map((route, index) => (
-          <li key={index} className="flex h-full">
-            <Link
-              href={route.href}
-              className="flex items-center text-lg hover:bg-amber-500/80 h-full p-4"
-            >
-              {route.label}
-            </Link>
-          </li>
-        ))}
+        {Object.values(ROUTES).map((route, index) => {
+          const isActive = pathname === route.href;
+
+          return (
+            <li key={index} className="flex h-full">
+              <Link
+                href={route.href}
+                className={`flex items-center text-lg hover:bg-amber-500/80 h-full p-4 ${
+                  isActive ? 'underline underline-offset-8' : ''
+                }`}
+              >
+                {route.label}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
